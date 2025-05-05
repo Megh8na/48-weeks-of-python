8dd1f08f4872f3a4b0bd40a286264bb9fa93f9fe
Megh8na <meghanagade8@gmail.com>
2025-05-05 13:59:14 -0500

    Week 01: variables and data types assignment

---
+++ Week01/Variablesandatatypes.py
@@ -0,0 +1,16 @@
+# Week 01 Assignment - Variables and data types
+
+# Declaration of variables
+
+name = "Meghana"
+age = 25
+height = 5.3
+
+# printing in a single sentence 
+
+print(f"My name is {name}, I am {age} years old, and my height is {height} feet.")
+
+# To check Data types
+
+print(f"Data type:\n name--> {type(name)}\n Age--> {type(age)}\n Height--> {type(height)}")
+
