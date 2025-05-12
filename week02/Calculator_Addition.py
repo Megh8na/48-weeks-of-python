f3fdf3a7ccbd963137791a1c6764eceaf27b8870
Megh8na <meghanagade8@gmail.com>
2025-05-12 11:01:14 -0500

    Week 2: User Input & String Formatting - Calculator

---
+++ week02/Calculator_Addition.py
@@ -0,0 +1,19 @@
+# Week02 - User inputs and Formatting
+
+# Taking inputs from users
+
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+# type casting
+
+num1 = float(a)
+num2 = float(b)
+
+# adding the numbers
+
+results = num1 + num2
+
+# displaying the output using f-string
+
+print(f"The sum of {a} and {b} is {results}")
