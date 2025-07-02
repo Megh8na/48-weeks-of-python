2954f836708eeaff17dd583caf75100efca08708
Megh8na <meghanagade8@gmail.com>
2025-07-02 14:16:57 -0500

    Week 9: Function to return square of a number

---
+++ Week09/square_function.py
@@ -0,0 +1,10 @@
+# Week 9: Writing Functions
+
+# Step 1: Define the function
+def square(number):
+    return number ** 2
+
+# Step 2: Test the function with 3 inputs
+print("Square of 3:", square(3))
+print("Square of 5:", square(5))
+print("Square of -7:", square(-7))
